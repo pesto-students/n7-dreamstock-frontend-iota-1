d9dbe9d20d4df5c30a2036411ae4b1913619d55f
AbhinavDegala <abhinav.degala@gmail.com>
2021-10-04 21:30:01 +0530

    feat: test commit

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "../components/Container";
+
+const AboutUs = (props) => {
+
+  return (
+    <Container flexCenter>
+
+    </Container>
+  );
+};
+
+export default AboutUs;
