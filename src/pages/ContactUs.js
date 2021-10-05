96000aed2246fff648b8cf2cc60c31cddc08ad8d
AbhinavDegala <abhinav.degala@gmail.com>
2021-10-06 00:44:44 +0530

    fix: testing build

---
+++ src/pages/ContactUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "../components/Container";
+
+const ContactUs = (props) => {
+
+  return (
+    <Container flexCenter>
+
+    </Container>
+  );
+};
+
+export default ContactUs;
